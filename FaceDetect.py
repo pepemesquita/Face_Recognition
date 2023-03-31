a1ad9398b6fa9ab7084d6ecedc9755c619f37ac4
Pedro <phlmesquita@inf.ufpel.edu.br>
2023-03-31 04:26:42 -0300

    FaceRecognition.py added and FaceDetect detect eyes now

--- FaceDetect.py
+++ FaceDetect.py
@@ -1,9 +1,12 @@
 import numpy as np
 import cv2
 faceCascade = cv2.CascadeClassifier('data/haarcascade_frontalface_default.xml')
+eyeCascade = cv2.CascadeClassifier('data/haarcascade_eye.xml')
+
 cap = cv2.VideoCapture(0)
 cap.set(3, 640) # set Width
 cap.set(4, 480) # set Height
+
 while True:
     ret, img = cap.read()
     img = cv2.flip(img, 1)
@@ -18,6 +21,11 @@ while True:
         cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
         roi_RGB = RGB[y:y+h, x:x+w]
         roi_color = img[y:y+h, x:x+w]
+
+        eyes = eyeCascade.detectMultiScale(roi_color)
+
+        for (ex, ey, ew, eh) in eyes:
+            cv2.rectangle(roi_color, (ex, ey), (ex + ew, ey + eh), (0, 255, 0), 2)
 
     cv2.imshow('Video', img)
     k = cv2.waitKey(30) & 0xff
