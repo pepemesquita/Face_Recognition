e83cdf10c0c1afd10fa8cd363c0970d57f0b807f
Pedro <phlmesquita@inf.ufpel.edu.br>
2023-03-31 03:22:08 -0300

    utilizando exemplos do Classifier

--- SimpleVideoCapture.py
+++ SimpleVideoCapture.py
@@ -1,15 +1,20 @@
-import numpy as np
 import cv2
 
-cap = cv2.VideoCapture(0)
+capture = cv2.VideoCapture(0)
+capture.set(3, 640)
+capture.set(4, 480)
 
-while (True):
-    ret, frame = cap.read()
+while True:
+    ret, frame = capture.read()
+    frame = cv2.flip(frame, 1)  # Flip the camera (if you want)
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    cv2.imshow('frame', gray)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    cv2.imshow('RGB', frame)  # Normal
+    cv2.imshow('GRAY', gray)  # Or gray scale
+
+    key = cv2.waitKey(30) & 0xFF
+    if key == 27:  # Press ESC to close the panel
         break
 
-cap.release()
+capture.release()
 cv2.destroyAllWindows()
